1100575d2fa1c7f687020e0f4ce30da4c746a023
Shubhangi Raj <shubhangi120989@gmail.com>
2023-10-26 09:20:51 +0530

    react router bug fix

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import React, { Component } from 'react';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import News from './components/News';
+import React, { Component } from 'react'
+import Navbar from './components/Navbar'
+import News from './components/News'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-class App extends Component {
+export class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Navbar />
-          <Route path="/" exact component={News} />
-        </div>
-      </Router>
-    );
+      <div>
+        <Router>
+        <Navbar/>
+        
+        <Switch>
+          <Route path="/"><News pagesize={9} country="in" category="general"/></Route>
+          <Route path="/business"><News pagesize={9} country="in" category="business"/></Route>
+          <Route path="/entertainment"><News pagesize={9} country="in" category="entertainment"/></Route>
+          <Route path="/general"><News pagesize={9} country="in" category="general"/></Route>
+          <Route path="/health"><News pagesize={9} country="in" category="health"/></Route>
+          <Route path="/science"><News pagesize={9} country="in" category="science"/></Route>
+          <Route path="/sports"><News pagesize={9} country="in" category="sports"/></Route>
+          <Route path="/technology"><News pagesize={9} country="in" category="technology"/></Route>
+        </Switch>
+        </Router>
+      </div>
+    )
   }
 }
 
-export default App;
+export default App
 
